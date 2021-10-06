08ee72fced39051d25190dbb8fd84b5884f1408f
Jonathan <jtellkampf@gmail.com>
2021-10-06 01:17:38 +0100

    Created chat component

---
+++ web/components/Chat.tsx
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+import { Avatar } from "@material-ui/core";
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  padding: 15px;
+  word-break: break-word;
+
+  :hover {
+    background-color: #e9eaeb;
+  }
+`;
+
+const UserAvatar = styled(Avatar)`
+  margin: 5px;
+  margin-right: 15px;
+`;
+
+const Chat: React.FC = () => {
+  return (
+    <Container>
+      <UserAvatar />
+      <p>Recipient Email</p>
+    </Container>
+  );
+};
+
+export default Chat;
