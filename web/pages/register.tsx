340457fc82f628d30203967f2e7961e77b221cac
Jonathan <jtellkampf@gmail.com>
2021-11-11 20:22:05 +0000

    Added more styles to register page

--- web/pages/register.tsx
+++ web/pages/register.tsx
@@ -1,11 +1,20 @@
 import styled from "styled-components";
 import WhatsAppIcon from "@material-ui/icons/WhatsApp";
 import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import Avatar from "@material-ui/core/Avatar";
+import NavigateNextIcon from "@material-ui/icons/NavigateNext";
+import NavigateBeforeIcon from "@material-ui/icons/NavigateBefore";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import SendIcon from "@material-ui/icons/Send";
+
+import { useState, FormEvent, Fragment } from "react";
 
 const Container = styled.div`
   display: flex;
   justify-content: center;
   padding: 77.5px 0;
+  height: 100vh;
   ${({ theme }) => theme.registerPageTheme.smallScreen`
     padding: 0;
   `};
@@ -27,9 +36,217 @@ const RegisterBox = styled.div`
   `};
 `;
 
-const Title = styled.h1``;
+const Title = styled.h1`
+  margin: 15px 0;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Box = styled.div`
+  margin-top: 15px;
+  text-align: center;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  flex-direction: row-reverse;
+  justify-content: space-between;
+  margin-top: 15px;
+`;
+
+const Input = styled.input`
+  display: none;
+`;
 
 const Register: React.FC = () => {
+  const [page, setPage] = useState(1);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [about, setAbout] = useState("");
+  const [profilePicture, setProfilePicture] = useState<File | null>(null);
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
+  const renderRegisterMultiPageForm = () => {
+    switch (page) {
+      case 1:
+      default:
+        return (
+          <Fragment>
+            <Box>
+              <TextField
+                onChange={(e) => setFirstName(e.target.value)}
+                value={firstName}
+                name="firstName"
+                placeholder="First Name"
+                label="First Name"
+                variant="outlined"
+                type="text"
+              />
+            </Box>
+            <Box>
+              <TextField
+                onChange={(e) => setLastName(e.target.value)}
+                value={lastName}
+                name="lastName"
+                placeholder="Last Name"
+                label="Last Name"
+                variant="outlined"
+                type="text"
+              />
+            </Box>
+            <ButtonContainer>
+              <Button
+                variant="outlined"
+                type="button"
+                onClick={() => setPage(2)}
+                endIcon={<NavigateNextIcon />}
+              >
+                Next
+              </Button>
+            </ButtonContainer>
+          </Fragment>
+        );
+      case 2:
+        return (
+          <Fragment>
+            <Box>
+              <TextField
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+                type="email"
+                placeholder="Email"
+                label="Email"
+                variant="outlined"
+                name="email"
+              />
+            </Box>
+            <Box>
+              <TextField
+                onChange={(e) => setUsername(e.target.value)}
+                value={username}
+                name="username"
+                placeholder="Username"
+                label="Username"
+                variant="outlined"
+                type="text"
+              />
+            </Box>
+            <Box>
+              <TextField
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+                name="password"
+                placeholder="Password"
+                label="Password"
+                variant="outlined"
+                type="password"
+              />
+            </Box>
+            <ButtonContainer>
+              <Button
+                variant="outlined"
+                type="button"
+                onClick={() => setPage(3)}
+                endIcon={<NavigateNextIcon />}
+              >
+                Next
+              </Button>
+              <Button
+                variant="outlined"
+                type="button"
+                onClick={() => setPage(1)}
+                startIcon={<NavigateBeforeIcon />}
+              >
+                Back
+              </Button>
+            </ButtonContainer>
+          </Fragment>
+        );
+      case 3:
+        return (
+          <Fragment>
+            <Box>
+              <TextField
+                variant="outlined"
+                id="outlined-multiline-flexible"
+                label="About"
+                name="about"
+                placeholder="Tell us about yourself..."
+                multiline
+                maxRows={4}
+                value={about}
+                onChange={(e) => setAbout(e.target.value)}
+              />
+              <Box>
+                <label htmlFor="contained-button-file">
+                  <Input
+                    accept="image/*"
+                    id="contained-button-file"
+                    multiple
+                    type="file"
+                    // onChange={(e) => setProfilePicture(e.target.files)}
+                    // value={profilePicture}
+                  />
+                  <Button
+                    startIcon={<AccountCircleIcon />}
+                    variant="outlined"
+                    component="span"
+                  >
+                    Upload
+                  </Button>
+                </label>
+              </Box>
+            </Box>
+            <ButtonContainer>
+              <Button
+                onClick={() => setPage(4)}
+                endIcon={<NavigateNextIcon />}
+                variant="outlined"
+                type="button"
+              >
+                Skip
+              </Button>
+              <Button
+                variant="outlined"
+                type="button"
+                onClick={() => setPage(2)}
+                startIcon={<NavigateBeforeIcon />}
+              >
+                Back
+              </Button>
+            </ButtonContainer>
+          </Fragment>
+        );
+      case 4:
+        return (
+          <Fragment>
+            <ButtonContainer>
+              <Button endIcon={<SendIcon />} variant="outlined" type="submit">
+                Submit
+              </Button>
+              <Button
+                variant="outlined"
+                type="button"
+                onClick={() => setPage(3)}
+                startIcon={<NavigateBeforeIcon />}
+              >
+                Back
+              </Button>
+            </ButtonContainer>
+          </Fragment>
+        );
+    }
+  };
+
   return (
     <Container>
       <RegisterBox>
@@ -41,11 +258,7 @@ const Register: React.FC = () => {
           }}
         />
         <Title>Register</Title>
-        <input name="firstName" placeholder="First Name" type="text" />
-        <input name="lastName" placeholder="Last Name" type="text" />
-        <input type="email" placeholder="Email" name="email" />
-        <input name="username" placeholder="Username" type="text" />
-        <input name="password" placeholder="password" type="password" />
+        <Form onSubmit={handleSubmit}>{renderRegisterMultiPageForm()}</Form>
       </RegisterBox>
     </Container>
   );
