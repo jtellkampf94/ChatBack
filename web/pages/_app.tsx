2332cb9e01a1cfe137bb73a1c5cf3f718a15ccc8
Jonathan <jtellkampf@gmail.com>
2021-12-28 20:46:12 +0000

    Configuered websocket link for apollo client to eneble subscriptions

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -4,22 +4,49 @@ import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
-  createHttpLink,
+  HttpLink,
+  split,
+  ApolloLink,
 } from "@apollo/client";
+import { getMainDefinition } from "@apollo/client/utilities";
+import { WebSocketLink } from "@apollo/client/link/ws";
 
 import { Theme } from "../themes";
 import { UserProvider } from "../context/UserContext";
 
 export const createApolloClient = (headers?: Record<string, string>) => {
-  const link = createHttpLink({
+  const httpLink = new HttpLink({
     uri: "http://localhost:4000/",
     credentials: "include",
     headers: headers || {},
   });
 
+  let splitLink: HttpLink | ApolloLink = httpLink;
+
+  if (process.browser) {
+    const wsLink = new WebSocketLink({
+      uri: "ws://localhost:4000/",
+      options: {
+        reconnect: true,
+      },
+    });
+
+    splitLink = split(
+      ({ query }) => {
+        const definition = getMainDefinition(query);
+        return (
+          definition.kind === "OperationDefinition" &&
+          definition.operation === "subscription"
+        );
+      },
+      wsLink,
+      httpLink
+    );
+  }
+
   return new ApolloClient({
     cache: new InMemoryCache(),
-    link,
+    link: splitLink,
   });
 };
 
