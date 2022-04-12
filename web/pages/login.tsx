ae5998df3fa32fb4b29ba01ec7d9f90097dc5ac4
Jonathan <jtellkampf@gmail.com>
2022-04-12 21:32:23 +0100

    Refactored Login page

--- web/pages/login.tsx
+++ web/pages/login.tsx
@@ -1,104 +1,27 @@
 import { NextPage } from "next";
-import styled from "styled-components";
 import Head from "next/head";
 import { useMediaQuery } from "react-responsive";
 
-import WhatsAppLogo from "../assets/images/whats-app-logo.svg";
 import LoginForm from "../containers/LoginForm";
-import DownloadOnTheAppStoreLogo from "../components/DownloadOnTheAppStoreLogo";
-import GooglePlayDownloadLogo from "../components/GooglePlayDownloadLogo";
+import LoginContainer from "../components/LoginContainer";
+import LoginHeaderSection from "../components/LoginHeaderSection";
+import LoginFormContainer from "../components/LoginFormContainer";
 import { userLoggedIn } from "../utils/isUserLoggedIn";
-
-const Container = styled.div`
-  display: flex;
-  height: 100vh;
-  width: 100vw;
-  background-color: ${({ theme }) => theme.globalTheme.white};
-`;
-
-const HeaderSection = styled.div`
-  width: 55%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  color: ${({ theme }) => theme.globalTheme.white};
-  padding: 100px;
-  background-color: ${({ theme }) => theme.globalTheme.darkGreen};
-`;
-
-const Heading = styled.h1`
-  font-size: 32px;
-  font-weight: 600;
-  margin-top: 40px;
-`;
-
-const Subheading = styled.h4`
-  font-size: 18px;
-  font-weight: 300;
-  margin-top: 10px;
-`;
-
-const HeaderSectionFooter = styled.div`
-  margin-top: 40px;
-`;
-
-const FooterText = styled.p`
-  font-size: 18px;
-  font-weight: 300;
-  margin-bottom: 20px;
-`;
-
-const IconsContainer = styled.div`
-  height: 50px;
-  display: flex;
-  align-items: center;
-`;
-
-const LoginSection = styled.div`
-  width: 45%;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  @media screen and (max-width: 880px) {
-    width: 100%;
-    background-color: ${({ theme }) => theme.globalTheme.smokeGrey};
-  }
-`;
 
 const Login: NextPage = () => {
   const isSmallScreen = useMediaQuery({ query: "(max-width: 880px)" });
   return (
-    <Container>
+    <LoginContainer>
       <Head>
         <title>Login</title>
       </Head>
 
-      {!isSmallScreen && (
-        <HeaderSection>
-          <WhatsAppLogo />
-          <Heading>
-            Simple. Secure. <br /> Reliable Messaging.
-          </Heading>
-          <Subheading>
-            With WhatsApp web, you'll get fast, simple and secure messaging on
-            your laptop or desktop.
-          </Subheading>
-          <HeaderSectionFooter>
-            <FooterText>Get the app.</FooterText>
-            <IconsContainer>
-              <DownloadOnTheAppStoreLogo width="136px" height="40px" />
-              <GooglePlayDownloadLogo width={150} height={44} />
-            </IconsContainer>
-          </HeaderSectionFooter>
-        </HeaderSection>
-      )}
+      {!isSmallScreen && <LoginHeaderSection />}
 
-      <LoginSection>
+      <LoginFormContainer>
         <LoginForm />
-      </LoginSection>
-    </Container>
+      </LoginFormContainer>
+    </LoginContainer>
   );
 };
 
